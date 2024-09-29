062f39f3ab4b506e68205bd208678cd17a39c09e
unknown <ashbinthapa2054@gmail.com>
2024-09-29 14:40:48 +0545

    publiaction page created

---
+++ resources/views/publication.blade.php
@@ -0,0 +1,13 @@
+<!-- resources/views/publication.blade.php -->
+@extends('layouts.app')
+
+@section('title', 'publication')
+
+@section('content')
+<div class="ashbin-section">
+    <p class="ashbin-title">PUBLICATIONS</p>
+</div>
+<div class="ashbin-section">
+    <p class="ashbin-subtitle">In Progress</p>
+</div>
+@endsection
