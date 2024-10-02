20af25b15a28595f99731aece258e3409b24a32b
unknown <ashbinthapa2054@gmail.com>
2024-10-02 15:09:01 +0545

    gallery view page created

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,155 @@
+<!-- resources/views/gallery.blade.php -->
+@extends('layouts.app')
+
+@section('title', 'gallery')
+
+@section('content')
+<div class="ashbin-section">
+    <p class="ashbin-title">GALLERY</p>
+    <div class="ashbin-section">
+        <div class="card">
+            <div class="card-body">
+                <p class="ashbin-title">WORKSHOP WITH THE LOCAL GOVERNMENT</p>
+                <div class="row ashbin-gallery-img">
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/1/DSCN0428_e-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/1/DSCN0430_e-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/1/DSCN0431_e-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                </div>
+                <div class="row ashbin-gallery-img">
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/1/DSCN0437_e-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <p class="ashbin-title">STAKEHOLDER'S WORKSHOPS</p>
+                <div class="row ashbin-gallery-img">
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/2/DSCN0472-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/2/DSCN0478-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/2/DSCN0486-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                </div>
+                <div class="row ashbin-gallery-img">
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/2/DSCN0491-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/2/DSCN0502-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/2/DSCN0558-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                </div>
+                <div class="row ashbin-gallery-img">
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/2/DSCN0566-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/2/DSCN0654-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/2/DSCN0668-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <p class="ashbin-title">STUDENT ACHIEVEMENT ASSESSMENT INVIGILATOR TRAINING</p>
+                <div class="row ashbin-gallery-img">
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/3/DSCN0229-Copy.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/3/DSCN0715.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/3/DSCN0720.jpg') }}" alt="Idea-Iser">
+                    </div>
+                </div>
+                <div class="row ashbin-gallery-img">
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/3/DSCN0728.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/3/DSCN0734.jpg') }}" alt="Idea-Iser">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <p class="ashbin-title">EXAMINER TRAINING ON THE STUDENT ACHIEVEMENT ASSESSMENT</p>
+                <div class="row ashbin-gallery-img">
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/4/DSCN0775.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/4/DSCN0777.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/4/DSCN0781.jpg') }}" alt="Idea-Iser">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <p class="ashbin-title">DATA ENTRY</p>
+                <div class="row ashbin-gallery-img">
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/5/data-entry-3.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/5/data-entry.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/5/data-entry2.jpg') }}" alt="Idea-Iser">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <p class="ashbin-title">GENERAL INTERVIEWING TECHNIQUE TRAINING TO NEWLY HIRED INTERVIEWERS</p>
+                <div class="row ashbin-gallery-img">
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/6/DSC01467.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/6/DSC01491.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/6/DSC01492.jpg') }}" alt="Idea-Iser">
+                    </div>
+                </div>
+                <div class="row ashbin-gallery-img">
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/6/DSC01630-1.jpg') }}" alt="Idea-Iser">
+                    </div>
+                    <div class="col-lg-4 col-12">
+                        <img src="{{ asset('/uploads/gallery/6/Post-Training.jpg') }}" alt="Idea-Iser">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
