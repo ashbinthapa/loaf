8d68cf267d765c274fd319a0114113dc81a6424d
unknown <ashbinthapa2054@gmail.com>
2024-10-07 10:40:46 +0545

    emails thank you page created

---
+++ resources/views/emails/thank-you.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/contact.blade.php -->
+@extends('layouts.app')
+
+@section('title', 'Thank You')
+
+@section('content')
+<div class="container">
+    <h1>Thank You!</h1>
+    <p>Your message has been sent successfully. We will get back to you soon.</p>
+</div>
+@endsection
