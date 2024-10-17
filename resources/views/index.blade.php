82cfa41805339d79b0cec579e3d01ca6eee2a29e
unknown <ashbinthapa2054@gmail.com>
2024-10-17 11:27:22 +0545

    slider images uploaded

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,19 +7,28 @@
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="{{ asset('uploads/idea-slider1.jpg') }}" class="d-block w-100" alt="...">
+            <img src="{{ asset('images/1.jpg') }}" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="{{ asset('uploads/idea-slider2.jpg') }}" class="d-block w-100" alt="...">
+            <img src="{{ asset('images/slider/2.jpg') }}" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="{{ asset('uploads/idea-slider3.jpg') }}" class="d-block w-100" alt="...">
+            <img src="{{ asset('images/slider/3.jpg') }}" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="{{ asset('uploads/idea-slider4.jpg') }}" class="d-block w-100" alt="...">
+            <img src="{{ asset('images/slider/4.jpg') }}" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-            <img src="{{ asset('uploads/idea-slider5.jpg') }}" class="d-block w-100" alt="...">
+            <img src="{{ asset('images/slider/5.jpg') }}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('images/slider/6.jpg') }}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('images/slider/7.jpg') }}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('images/slider/8.jpg') }}" class="d-block w-100" alt="...">
         </div>
     </div>
 </div>
